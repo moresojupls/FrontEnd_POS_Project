f4c73e552b2e9be140334a169cb38eb5e29a0200
Bhunmas <lovebhunma@outlook.com>
2024-10-17 19:34:12 +0700

    The First NavBar

--- src/assets/Components/Navbar/navbar.jsx
+++ src/assets/Components/Navbar/navbar.jsx
@@ -1,10 +1,10 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import 'bootstrap/dist/css/bootstrap.css';
 
-import React, { useState } from 'react'
+
 import Myimg from '../Image/img'
 import Logo from './MilkTea_logo.png';
 
@@ -16,33 +16,39 @@ function Mynavbar() {
     //     setDate(newDate.getHours()+' : '+newDate.getMinutes()+' : '+newDate.getSeconds())
     // },1000)
   return (
-    <div>
-    <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
-      <Container>
-        <Myimg url={Logo} size={"80px"} />
-        <Navbar.Brand href="#home"c>MilkTea</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {/* <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Text  />
-        {/* <h1>{date   }</h1> */}
-      </Container>
-      </Navbar>
-    </div>
+  
+      
+      <Navbar bg="prinary  "   expand="lg" className='d-flex '  fixed="top">
+       
+          
+          <Navbar.Brand className='d-flex flex-row' href="home">
+            <Myimg url={Logo} size={"40px"} />
+            <h2>Welcome</h2>
+            
+          </Navbar.Brand>
+          <Container className='d-flex justify-content-end'>
+            <Navbar.Text className='mx-4'>
+                <div>
+                  <h3>Time</h3>
+                </div>
+            </Navbar.Text>
+
+            <Navbar.Text className='mx-4'>
+                <div>
+                  <h3>User</h3>
+                </div>
+            </Navbar.Text>
+          </Container>
+          
+            
+        
+         
+          
+       
+        
+   
+        </Navbar>
+   
   )
 }
 
