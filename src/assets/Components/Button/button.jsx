94e1ef54990b5c7000fffe5c2d58daa86a004922
Bhunmas <lovebhunma@outlook.com>
2024-10-17 17:58:24 +0700

    default code2

--- src/assets/Components/Button/button.jsx
+++ src/assets/Components/Button/button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Mybutton({topic,process}) {
   return (
-    <button style={{minWidth:'150px'}} onClick={process}>  {topic}dasdasd</button>
+    <button style={{minWidth:'150px'}} onClick={process}>  {topic}</button>
   )
 }
 
