926072b808ea4cef8f7d7ab418eaa2f11b1338aa
Bhunmas <lovebhunma@outlook.com>
2024-10-18 16:36:06 +0700

    navbar create time

--- src/assets/Components/Navbar/navbar.jsx
+++ src/assets/Components/Navbar/navbar.jsx
@@ -7,8 +7,31 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import Myimg from '../Image/img'
 import Logo from './MilkTea_logo.png';
+import { useState,useEffect } from 'react';
 
 function Mynavbar() {
+  function UserDisplay(){
+    const data = {
+      name:'champ',
+      gender:'man'
+    }
+    return `${data.gender === 'man'?'Mr':'Miss'}  ${data.name}`;
+  }
+  function DateNow(){
+    const date = new Date();
+    const day = `${ date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    return day+time
+  }
+  const [date,setDate] = useState(0);
+  useEffect(()=>{
+    setTimeout(()=>{
+        setDate(()=>
+          DateNow()
+        
+        )
+    },1000)
+  })
     // const [date,setDate] = useState();
     // var newDate = new Date();
 
@@ -29,13 +52,13 @@ function Mynavbar() {
           <Container className='d-flex justify-content-end'>
             <Navbar.Text className='mx-4'>
                 <div>
-                  <h3>Time</h3>
+                  <h5>Time {date}</h5>
                 </div>
             </Navbar.Text>
 
             <Navbar.Text className='mx-4'>
                 <div>
-                  <h3>User</h3>
+                  <h5>User {UserDisplay()}</h5>
                 </div>
             </Navbar.Text>
           </Container>
