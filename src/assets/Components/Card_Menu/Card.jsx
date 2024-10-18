528360eee4d501874285d5b526443f02df881708
moresojupls <champsadayu@gmail.com>
2024-10-18 18:25:16 +0700

    Card_Menu_1st

--- src/assets/Components/Card_Menu/Card.jsx
+++ src/assets/Components/Card_Menu/Card.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import '../../../MyCard.css'
 
-function Card() {
+function MyCard({myimg,name,price}) {
   return (
-    <div>
-      
+    <div className="card-wrapper">
+      <div className="card">
+        <img src={myimg} alt="Name" style={{display: 'flex' ,width: "100px" , borderRadius:"8px" }} />
+          <h6> {name} </h6>
+          <h3><b> {price} </b> </h3>
+      </div>
     </div>
   )
 }
 
-export default Card
+export default MyCard
