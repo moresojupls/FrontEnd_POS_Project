28ff5560cb106b5af895afb9fd75750ab9311745
Bhunmas <lovebhunma@outlook.com>
2025-01-05 20:07:53 +0700

    config address

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/FrontEnd_POS_Project',
-  server:{
-    host:'127.0.0.1',
-    port:4432
-  }
+  preview: {
+   port: 5173,
+   strictPort: true,
+  },
+  server: {
+   port: 5173,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:3000",
+  },
+
 })
